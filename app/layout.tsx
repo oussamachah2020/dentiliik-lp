07b5cba8ae2d2758249371cfdb64461b3f0681be
oussamachah2020 <otaksam213@gmail.com>
2025-08-20 13:37:33 +0100

    adding logo

--- app/layout.tsx
+++ app/layout.tsx
@@ -80,19 +80,7 @@ export const metadata: Metadata = {
   generator: "Next.js",
   manifest: "/manifest.json",
   icons: {
-    icon: [
-      { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
-      { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
-    ],
-    apple: [
-      { url: "/apple-touch-icon.png", sizes: "180x180", type: "image/png" },
-    ],
-    other: [
-      {
-        rel: "apple-touch-icon-precomposed",
-        url: "/apple-touch-icon-precomposed.png",
-      },
-    ],
+    icon: "/logo.svg",
   },
   appleWebApp: {
     title: "Dentillik",
--- components/Header.tsx
+++ components/Header.tsx
@@ -1,8 +1,10 @@
 "use client"
 
 import { useState, useEffect } from "react";
-import { Menu, X, Stethoscope } from "lucide-react"
-import { Button } from "@/components/ui/button"
+import { Menu, X } from "lucide-react";
+import { Button } from "@/components/ui/button";
+import Logo from "@/public/logo.svg";
+import Image from "next/image";
 
 export default function Header() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
@@ -31,9 +33,7 @@ export default function Header() {
       <div className="px-4 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between h-16">
           <div className="flex items-center space-x-2 group">
-            <div className="w-8 h-8 bg-gradient-to-r from-violet-600 to-purple-600 rounded-xl flex items-center justify-center shadow-lg group-hover:shadow-xl transition-all duration-300 group-hover:scale-110">
-              <Stethoscope className="w-4 h-4 text-white" />
-            </div>
+            <Image src={Logo.src} alt="Logo" height={40} width={40} />
             <span className="text-xl font-bold bg-gradient-to-r from-violet-600 to-purple-600 bg-clip-text text-transparent">
               Dentillik
             </span>
